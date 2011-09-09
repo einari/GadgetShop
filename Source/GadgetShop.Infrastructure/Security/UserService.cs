ce3396563740eb8afb5e3b46637a4ae454e8ae9a
Einar Ingebrigtsen <einar@dolittle.com>
2011-09-09 14:37:51 +0200

    Simple UserService

--- Source/GadgetShop.Infrastructure/Security/UserService.cs
+++ Source/GadgetShop.Infrastructure/Security/UserService.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Web;
 
 namespace GadgetShop.Infrastructure.Security
 {
     public class UserService : IUserService
     {
+        const string CurrentUserKey = "CurrentUser";
+
         public Guid GetCurrentUserId()
         {
-            throw new NotImplementedException();
+            Guid userId = Guid.Empty;
+            var keyFromSession = HttpContext.Current.Session[CurrentUserKey];
+            if (keyFromSession != null)
+            {
+                userId = (Guid)keyFromSession;
+            }
+            else
+            {
+                userId = Guid.NewGuid();
+                HttpContext.Current.Session[CurrentUserKey] = userId;
+            }
+
+            return userId;
         }
     }
 }
